99f7df5ed388e021c6c1d570fe930d8931fd600d
carlinhocr <67878094+carlinhocr@users.noreply.github.com>
2020-07-24 10:56:43 -0300

    Update raw_tx_demo.js
    correct how ethereumjs-tx 2.0 is now called and the way it process parameters now

--- code/web3js/raw_tx/raw_tx_demo.js
+++ code/web3js/raw_tx/raw_tx_demo.js
@@ -4,7 +4,7 @@
 // npm install ethereumjs-tx
 //
 // Run with: $ node raw_tx_demo.js
-const ethTx = require('ethereumjs-tx');
+const ethTx = require('ethereumjs-tx').Transaction;
 
 const txData = {
   nonce: '0x0',
@@ -12,10 +12,8 @@ const txData = {
   gasLimit: '0x30000',
   to: '0xb0920c523d582040f2bcb1bd7fb1c7c1ecebdb34',
   value: '0x00',
-  data: '',
+  data: '0x00',
   v: "0x1c", // Ethereum mainnet chainID
-  r: 0,
-  s: 0 
 };
 
 tx = new ethTx(txData);
